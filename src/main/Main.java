27513abc662784660b78b0ff45b5c9071e22bcb4
Arno Alexander <arno2alexander@gmail.com>
2018-03-30 08:17:05 +0700

    Add input handling

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,27 @@
 package main;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
+	
 	public static void main(String[] args) {
-		System.out.println("Test Print");
+		
+		Scanner scanner = null;
+		if (args.length == 0) {
+			scanner = new Scanner(System.in);
+		} else {
+			try {
+				scanner = new Scanner(new File(args[0]));
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		while (scanner.hasNext()) {
+			System.out.println(scanner.nextLine());
+		}
 	}
+	
 }
